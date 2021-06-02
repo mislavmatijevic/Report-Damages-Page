0e7cf01a2160ce7ae8879f490d2e0fed6a0095b3
Seljumnik <bixi36@gmail.com>
2021-06-02 14:07:34 +0200

    Ultra slick dizajn.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(() => {
+
+    $("#hamburger_menu").on("click", (e) => {
+
+        var a = $(".header__nav-item-hambi");
+        if (a.length === 0) {
+
+            $("#hamburger_menu").toggleClass("header__nav__hamburger-active");
+            setTimeout(() => {
+                $(".header__nav-item").toggleClass("header__nav-item-hambi");
+            }, 50);
+
+        } else {
+            
+            $(".header__nav-item").toggleClass("header__nav-item-hambi");
+            setTimeout(() => {
+                $("#hamburger_menu").toggleClass("header__nav__hamburger-active");
+            }, 50);
+
+        }
+
+    });
+
+
+})
